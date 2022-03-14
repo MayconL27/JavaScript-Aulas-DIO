34d7f8c2f5b529e3de09ca5058b87812776514aa
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-03-14 19:37:27 -0300

    Aula 05

---
+++ Aula-05-Funções/script.js
@@ -0,0 +1,13 @@
+/*
+function funcao() {
+    console.log('Mensagem usando functon');
+}
+
+fuction();
+*/
+
+function funcao(paramento1, paramento2) {
+    console.log(paramento1, paramento2);
+}
+
+funcao('Maycon', 'Leandro');
