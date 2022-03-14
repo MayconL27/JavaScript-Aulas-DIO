eae3b7edb2f7f3993e0c5b39322aa36f2d83bf6a
Maycon <57063866+MayconL27@users.noreply.github.com>
2022-03-14 20:16:38 -0300

    Aula 06

---
+++ Aula-06-Functions/tiposDeFunçao.js
@@ -0,0 +1,22 @@
+/*
+function funcao() {
+    console.log('função declarativa');
+    
+}
+funcao();
+
+*/
+
+// expressão de função
+/*
+var funcao = function () {
+    console.log('função');
+}
+funcao();
+*/
+
+// arrow function
+var funcao = () => {
+    console.log('arrow function');
+}
+funcao();
